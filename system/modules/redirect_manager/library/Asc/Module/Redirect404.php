debe82ed8f6f1b2b8a8e8264fca7a2bb8e6d20e2
asconsulting <andrew@andrewstevens.consulting>
2019-01-31 03:46:53 -0500

    Directory Redirect

--- system/modules/redirect_manager/library/Asc/Module/Redirect404.php
+++ system/modules/redirect_manager/library/Asc/Module/Redirect404.php
@@ -87,10 +87,7 @@ class Redirect404 extends \Contao\Module
 						
 						case "directory":
 							$strRedirect = trim($objRedirect->redirect, "/");
-							echo $strRedirect ."<br>";
-							echo substr(\Environment::get('request'), 0, strlen($strRedirect)) ."<br>";
 							if (substr(\Environment::get('request'), 0, strlen($strRedirect)) == $strRedirect && (substr(\Environment::get('request'), strlen($strRedirect), 1) == "/" || \Environment::get('request') == $strRedirect)) {
-								echo $strRedirect ."<br>";
 								if ($objRedirect->target_url) {
 									$strTarget = trim($objRedirect->target_url, "/");
 									$redirect = $strTarget .substr(\Environment::get('request'), strlen($strRedirect));
