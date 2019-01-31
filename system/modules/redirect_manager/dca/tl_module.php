76c153278bd5e2fc6695073d4281226bde752e5b
asconsulting <andrew@andrewstevens.consulting>
2019-01-30 21:37:13 -0500

    Frontend Module

---
+++ system/modules/redirect_manager/dca/tl_module.php
@@ -0,0 +1,16 @@
+<?php
+ 
+/**
+ * Redirect Manager
+ *
+ * Copyright (C) 2019 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/redirect_manager
+ * @link       https://andrewstevens.consulting
+ */
+
+ 
+/**
+ * Add a palette to tl_module
+ */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['redirect_404'] 	= '{title_legend},name,type';
--- system/modules/redirect_manager/languages/en/modules.php
+++ system/modules/redirect_manager/languages/en/modules.php
@@ -10,6 +10,13 @@
  */
  
  
+ /**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['redirect_manager'] 	= "Redirect Manager";
+$GLOBALS['TL_LANG']['FMD']['redirect_404']		= array('Redirect 404', 'Place this module on your 404 page to enable redirect manager.');
+
+
 /**
  * Back end modules
  */
