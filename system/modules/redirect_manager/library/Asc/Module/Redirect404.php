7d061f38c24e0940e5664ec4349f7b1516b77265
asconsulting <andrew@andrewstevens.consulting>
2019-02-01 00:27:40 -0500

    Trigger

--- system/modules/redirect_manager/library/Asc/Module/Redirect404.php
+++ system/modules/redirect_manager/library/Asc/Module/Redirect404.php
@@ -138,7 +138,7 @@ class Redirect404 extends \Contao\Module
 							}
 							
 							if ($strRedirectDomain == \Environment::get('host')) {
-								
+								echo "<hr><h4>Trigger</h4>";
 								if ($objRedirect->target_url != "") {
 									if ($strTargetProtocol != $strRedirectProtocol && $strTargetDomain != $strRedirectDomain) {
 										$redirect = $strTargetProtocol .'://' .$strTargetDomain .'/' .\Environment::get('request');
